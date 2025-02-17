85b0ade551585d07c0e5d0c5b98252ba861fbbaa
Behruzbek Mahmudjonov <behruzbekmahmudjonov08@gmail.com>
2025-02-17 18:48:25 +0500

    init commit

---
+++ src/main/java/uz/pdp/delivery_service_project/DeliveryServiceProjectApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.delivery_service_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryServiceProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryServiceProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/pdp/delivery_service_project/DeliveryServiceProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.delivery_service_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryServiceProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
